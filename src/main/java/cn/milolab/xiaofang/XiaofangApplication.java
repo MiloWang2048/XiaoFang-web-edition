062af1ffc2959c3d08ad5a7cfffd4cbac12c8796
milowang <milowang2048@foxmail.com>
2020-09-03 00:03:45 +0800

    添加注释

--- src/main/java/cn/milolab/xiaofang/XiaofangApplication.java
+++ src/main/java/cn/milolab/xiaofang/XiaofangApplication.java
@@ -5,19 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
-
-@SpringBootApplication
-
-// DAO接口包名
-@MapperScan("cn.milolab.xiaofang.dao")
-
-// web filter扫描
-@ServletComponentScan(basePackages = {"cn.milolab.xiaofang.filter"})
-
 /**
- * 小芳便利店主类
  * @author milow
  */
+@SpringBootApplication
+@MapperScan("cn.milolab.xiaofang.dao")
+@ServletComponentScan(basePackages = {"cn.milolab.xiaofang.filter"})
 public class XiaofangApplication {
 
     public static void main(String[] args) {
--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,33 +1,12 @@
-/**
- * @author milowang
- */
-
-/**
- * 展示提示框
- * @param text 警告框的文本
- * @returns {Promise<>} 标志成功和失败的promise
- */
-function showAlert(text) {
-
-    // 返回一个一定会成功的promise
-    return new Promise(resolve => {
-
-        // 找到alert元素，设定到初始位置，变为可见
+function showAlert(text){
+    return new Promise(resolve=>{
         $(".xf-alert").css("bottom", "10vh").text(text).show().animate({
-
-            // 动画变换到指定位置
             bottom: '15vh'
-
-            // 时长为100ms
-        }, 100, () => {
-
-            // 倒数两秒，淡出提示框
-            setTimeout(() => {
+        }, 100, ()=>{
+            setTimeout(()=>{
                 $(".xf-alert").fadeOut();
-
-                // resolve promise
                 resolve();
-            }, 2000);
+            },2000);
         })
     })
 
