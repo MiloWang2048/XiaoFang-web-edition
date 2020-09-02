8a5895bdb0e4648c3c446cd89cf6ad13f080dae8
MiloWang <milowang2048@foxmail.com>
2020-09-02 14:17:44 +0800

    添加session初始化器

---
+++ src/main/java/cn/milolab/xiaofang/filter/SessionInitFilter.java
@@ -0,0 +1,28 @@
+package cn.milolab.xiaofang.filter;
+
+import cn.milolab.xiaofang.bean.business.BasketBO;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * 初始化session
+ * @author milowang
+ */
+@WebFilter(filterName = "sessionInit", urlPatterns = "/*")
+public class SessionInitFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        var session = ((HttpServletRequest) servletRequest).getSession();
+        var basketObject = session.getAttribute("basket");
+        if(!(basketObject instanceof BasketBO)){
+            basketObject = new BasketBO();
+        }
+        var basket = (BasketBO) basketObject;
+        session.setAttribute("basket", basket);
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
