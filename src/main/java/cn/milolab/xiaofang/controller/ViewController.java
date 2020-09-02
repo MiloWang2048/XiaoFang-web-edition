95903b3779e76532d6c8e6efa44c85f61ab0c847
milowang <milowang2048@foxmail.com>
2020-09-02 08:30:27 +0800

    初始化jquery和bootstrap

--- src/main/java/cn/milolab/xiaofang/controller/ViewController.java
+++ src/main/java/cn/milolab/xiaofang/controller/ViewController.java
@@ -9,9 +9,15 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class ViewController {
-    @GetMapping({"", "index.html", "index"})
+    @GetMapping({"", "index"})
     public String indexView(Model model){
         model.addAttribute("hello", "hello, thymeleaf!");
         return "index";
+    }
+
+    @GetMapping({"/basket"})
+    public String basketView(Model model){
+        model.addAttribute("index", true);
+        return "basket";
     }
 }
